c7e7a8e122a35a9a74619e4a17f7ed6c34616c50
Ben Harris <leet_haker@cyber-wizard.com>
2021-11-09 22:19:21 +0000

    Fix vec copy error, change Particle.update()

--- src/main.rs
+++ src/main.rs
@@ -71,14 +71,20 @@ pub struct ParticleSystem {
 
 impl ParticleSystem {
 
-    fn update(self, acceleration: VecTwo, dt: f32) {
-        for particle in &mut self.particles {
+    fn update(self, acceleration: VecTwo, dt: f32) -> ParticleSystem {
 
-            particle.velocity = particle.velocity + particle.acceleration * dt;
-            particle.position = particle.position + particle.velocity * dt;
+        let mut new_particles = Vec::<Particle>::new();
+        for particle in self.particles {
+            let mut new_particle = particle;
+
+            new_particle.velocity = particle.velocity + particle.acceleration * dt;
+            new_particle.position = particle.position + particle.velocity * dt;
 
 //            particle.check_collisions(bounding=self.size);
+            new_particles.push(new_particle)
         }
+
+        return ParticleSystem {size: self.size, particles: new_particles}
     }
 
 }
@@ -94,27 +100,23 @@ fn norm(a: VecTwo) -> f32 {
 }
 
 
-fn calculate_resultant_velocities(mut A: Particle, mut B: Particle) -> (Particle, Particle) {
+fn calculate_resultant_velocities(mut a: Particle, mut b: Particle) -> (Particle, Particle) {
 
-    let A_new_vel: VecTwo = A.velocity - (A.position - B.position) * (dot(A.velocity - B.velocity, A.position - B.position) / norm(A.position-B.position).powf(2.0)) * ((2.0*B.mass)/(A.mass + B.mass));
+    let a_new_vel: VecTwo = a.velocity - (a.position - b.position) * (dot(a.velocity - b.velocity, a.position - b.position) / norm(a.position-b.position).powf(2.0)) * ((2.0*b.mass)/(a.mass + b.mass));
 
-    let B_new_vel: VecTwo = B.velocity - (B.position - A.position) * (dot(B.velocity - A.velocity, B.position - A.position) / norm(B.position-A.position).powf(2.0)) * ((2.0*A.mass)/(A.mass + B.mass));
+    let a_new_vel: VecTwo = b.velocity - (b.position - a.position) * (dot(b.velocity - a.velocity, b.position - a.position) / norm(b.position-a.position).powf(2.0)) * ((2.0*a.mass)/(a.mass + b.mass));
 
 
-    A.velocity = A_new_vel;
-    B.velocity = B_new_vel;
+    a.velocity = a_new_vel;
+    b.velocity = a_new_vel;
 
-    return (A, B);
+    return (a, b);
 }
 
 fn main() {
-    let mut A: Particle = Particle {mass: 50.0, radius:1.0, velocity: VecTwo {x:50.0, y:50.0}, acceleration: VecTwo {x:0.0, y:0.0}, position: VecTwo {x:100.0, y:100.0}};
-    let mut B: Particle = Particle {mass: 50.0, radius:1.8, velocity: VecTwo {x:-50.0, y:-50.0}, acceleration: VecTwo {x:0.0, y:0.0}, position: VecTwo {x:170.0, y:170.0}};
-    let mut particle_system = ParticleSystem {size: VecTwo {x: 100.0, y: 100.0}, particles: vec![A, B]};
+    let a: Particle = Particle {mass: 50.0, radius:1.0, velocity: VecTwo {x:50.0, y:50.0}, acceleration: VecTwo {x:0.0, y:0.0}, position: VecTwo {x:100.0, y:100.0}};
+    let b: Particle = Particle {mass: 50.0, radius:1.8, velocity: VecTwo {x:-50.0, y:-50.0}, acceleration: VecTwo {x:0.0, y:0.0}, position: VecTwo {x:170.0, y:170.0}};
+    let mut particle_system = ParticleSystem {size: VecTwo {x: 100.0, y: 100.0}, particles: vec![a, b]};
 
-    println!("{}, {}", &particle_system.particles[0].position.x, &particle_system.particles[0].position.y);
-    println!("{}, {}", &particle_system.particles[1].position.x, &particle_system.particles[0].position.y);
-    particle_system.update(VecTwo{x:0.0, y:0.0}, 1.0/60.0);
-    println!("{}, {}", &particle_system.particles[0].position.x, &particle_system.particles[0].position.y);
-    println!("{}, {}", &particle_system.particles[1].position.x, &particle_system.particles[0].position.y);
+    particle_system = particle_system.update(VecTwo{x:0.0, y:0.0}, 1.0/60.0);
 }
